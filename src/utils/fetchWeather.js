60a271558376885588b5d233a1edf7de07045853
arhamathar <arhamathar789@gmail.com>
2021-01-19 20:52:53 +0530

    bug in url fixed

--- src/utils/fetchWeather.js
+++ src/utils/fetchWeather.js
@@ -1,20 +1,17 @@
-const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = process.env.REACT_APP_API_KEY;
+const units = 'metric';
 
 export const fetchWeather = async (place) => {
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=${units}&appid=${API_KEY}`;
     try {
-        const response = await fetch(URL, {
-            params: {
-                q: place,
-                units: 'metric',
-                APPID: API_KEY,
-            }
-        });
+        const response = await fetch(URL);
         const responseData = await response.json()
         return { responseData }
 
     } catch (err) {
         console.log(err);
+        const message = err.message;
+        return { message }
     }
 
 };
