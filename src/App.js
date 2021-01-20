ef3fcda78223000c0bb1a329f03704c5f9d3737a
arhamathar <arhamathar789@gmail.com>
2021-01-20 12:27:26 +0530

    info added to the card

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import './App.css';
 
 function App() {
     const [city, setCity] = useState('');
+    const [weather, setWeather] = useState({});
 
     const onSearchHandler = async (e) => {
         if (e.key === "Enter") {
             const data = await fetchWeather(city);
-            console.log(data);
+            setWeather(data.responseData);
+            setCity('');
+            console.log(data.responseData);
         }
     }
 
@@ -22,7 +25,28 @@ function App() {
                 value={city}
                 onChange={(e) => { setCity(e.target.value) }}
                 onKeyPress={onSearchHandler}
+                autoFocus
             />
+            {weather.main && (
+                <div className="city">
+                    <h2 className="city-name">
+                        <span>{weather.name}</span>
+                        <sup>{weather.sys.country}</sup>
+                    </h2>
+                    <div className="city-temp">
+                        {weather.main.temp}
+                        <sup>&deg;C</sup>
+                    </div>
+                    <div className="info">
+                        <img
+                            className="city-icon"
+                            src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                            alt={weather.weather[0].description}
+                        />
+                        <p>{weather.weather[0].main}</p>
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
